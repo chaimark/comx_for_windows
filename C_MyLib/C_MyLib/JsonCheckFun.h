15039b4f2d4d22b36ac7662440b672766a62acc3
chai21sks <chai21sks@gmail.com>
2026-05-11 01:01:37 +0800

    Mon May 11 01:01:37 CST 2026

---
+++ C_MyLib/C_MyLib/JsonCheckFun.h
@@ -0,0 +1,10 @@
+#ifndef __JSONCHECKFUN__
+#define __JSONCHECKFUN__
+
+#include "JsonDataAnalyzeLib.h"
+#include "JsonSetLib.h"
+#include "StrLib.h"
+
+extern void addCsToJsonAndPushJsonStr(JsonObject InputJsonStrObj);
+extern bool checkOfCsJsonStrIsRight(strnew JsonInputStr, strnew JsonOutputStr);
+#endif
